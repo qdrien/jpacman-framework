96db6514fdf89f9402da036ea0f3d2ee74212fed
NicolasLeemans7 <nicolas.LEEMANS@student.umons.ac.be>
2016-03-12 15:20:38 +0100

    Nettoyage du code

--- src/main/java/nl/tudelft/jpacman/Launcher.java
+++ src/main/java/nl/tudelft/jpacman/Launcher.java
@@ -1,9 +1,6 @@
 package nl.tudelft.jpacman;
 
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.InputStream;
