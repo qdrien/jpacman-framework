7bb39695136a592ef589453c089ac0f7b8858082
Damien Legay <Legay.Damien@hotmail.com>
2016-04-03 16:16:52 +0200

    Split Player & IdentifiedPlayer tests and switch from @Before to @BeforeClass initialisations.

--- src/test/java/nl/tudelft/jpacman/level/IdentifiedPlayerTest.java
+++ src/test/java/nl/tudelft/jpacman/level/IdentifiedPlayerTest.java
@@ -6,6 +6,7 @@ import nl.tudelft.jpacman.npc.ghost.Ghost;
 import nl.tudelft.jpacman.npc.ghost.GhostColor;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
@@ -21,7 +22,6 @@ import static org.mockito.Mockito.when;
  */
 public class IdentifiedPlayerTest
 {
-
     /**
      * The path of the file that will be used to test the testPlayer's profile.
      */
@@ -29,7 +29,7 @@ public class IdentifiedPlayerTest
     /**
      * The testPlayer we are making tests on
      */
-    private IdentifiedPlayer player;
+    private static IdentifiedPlayer player;
 
     /**
      * Deletes the file used to test profiles after all tests have run.
@@ -39,8 +39,8 @@ public class IdentifiedPlayerTest
         new File(PATH).delete();
     }
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() throws Exception {
         final Launcher launcher = new Launcher();
         launcher.launch();
         player = launcher.getGame().getPlayers().get(0);
@@ -51,50 +51,6 @@ public class IdentifiedPlayerTest
         final BufferedWriter writer = new BufferedWriter(new FileWriter(PATH));
         writer.write("0 0 0 0 0 0 0 0" + System.getProperty("line.separator"));
         writer.close();
-    }
-
-    /**
-     * Tests that Player::addPoints(int) effectively adds points and that a life is added when the threshold is reached
-     */
-    @Test
-    public void addPoints() {
-        final int score = player.getScore(), lives = player.getLives();
-        player.addPoints(10);
-        assertEquals(score + 10, player.getScore());
-        //using MAX_VALUE to ensure it is bigger than the "add life threshold"
-        player.addPoints(Integer.MAX_VALUE);
-        assertEquals(lives + 1, player.getLives());
-    }
-
-    /**
-     * Tests that Player::loseLife() effectively removes one life from the testPlayer when called
-     */
-    @Test
-    public void loseLife() {
-        final int lives = player.getLives();
-        player.loseLife();
-        assertEquals(lives - 1, player.getLives());
-    }
-
-    /**
-     * Test that a testPlayer can die "for good" when Player::loseLife() removes its last life
-     */
-    @Test
-    public void dies() {
-        player.setLives(1);
-        assert player.isAlive();
-        player.loseLife();
-        assertFalse(player.isAlive());
-    }
-
-    /**
-     * Tests that Player::addLife() effectively adds one life to the testPlayer when called
-     */
-    @Test
-    public void addLife() {
-        final int lives = player.getLives();
-        player.addLife();
-        assertEquals(lives + 1, player.getLives());
     }
 
     /**
---
+++ src/test/java/nl/tudelft/jpacman/level/PlayerTest.java
@@ -0,0 +1,68 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.Launcher;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class PlayerTest
+{
+    /**
+     * Player with which to run tests.
+     */
+    private static Player player;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        final Launcher launcher = new Launcher();
+        launcher.launch();
+        player = launcher.getGame().getPlayers().get(0);
+    }
+
+        /**
+         * Tests that Player::addPoints(int) effectively adds points and that a life is added when the threshold is reached
+         */
+    @Test
+    public void addPoints() {
+        final int score = player.getScore(), lives = player.getLives();
+        player.addPoints(10);
+        assertEquals(score + 10, player.getScore());
+        //using MAX_VALUE to ensure it is bigger than the "add life threshold"
+        player.addPoints(Integer.MAX_VALUE);
+        assertEquals(lives + 1, player.getLives());
+    }
+
+    /**
+     * Tests that Player::loseLife() effectively removes one life from the testPlayer when called
+     */
+    @Test
+    public void loseLife() {
+        final int lives = player.getLives();
+        player.loseLife();
+        assertEquals(lives - 1, player.getLives());
+    }
+
+    /**
+     * Test that a testPlayer can die "for good" when Player::loseLife() removes its last life
+     */
+    @Test
+    public void dies() {
+        player.setLives(1);
+        assert player.isAlive();
+        player.loseLife();
+        assertFalse(player.isAlive());
+    }
+
+    /**
+     * Tests that Player::addLife() effectively adds one life to the testPlayer when called
+     */
+    @Test
+    public void addLife() {
+        final int lives = player.getLives();
+        player.addLife();
+        assertEquals(lives + 1, player.getLives());
+    }
+}
